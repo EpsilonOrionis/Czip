9ae34699ae10733c5c3ba7432f1ca7ea9446d02e
Mirko Buttazzo <mirko9662@gmail.com>
2017-01-31 13:37:59 +0100

    Added menu and other choices

--- main.c
+++ main.c
@@ -7,20 +7,70 @@
 #include <stdlib.h>
 #include <dirent.h> // Libreria utilizzata per visualizzare i files nella cartella
 
+// Prototipo della funzione printContentDirectory
+void printContentDirectory();
+
 // Inizio funzione main
 int main(int argc, char** argv) {
+	
+	// Variabile per la scelta del menu
+	int menu_choice = 0;
+	
+	// Stampa un menu per chiedere cosa fare
+   do {
+		
+		printf("1) Mostra contenuto della cartella\n");
+		printf("2) Aggiungi file ad archivio\n");
+		printf("3) Rimuovi file ad archivio\n");
+		printf("4) Genera archivio .zip\n");
+		printf("9) Esci\n\n");
+		printf("Scegli una delle voci elencate: ");
+		scanf("%d", &menu_choice);
+				
+		switch(menu_choice) {
+		
+			case 1: {
+				
+				printContentDirectory();
+				menu_choice = 0;
+				break;
+			}
+			
+			case 9: {
+				
+				printf("Czip by Mirko Buttazzo (mirko9662@gmail.com)");
+				break;
+			}
+			
+			default: {
+				
+				printf("Operazione non consentita\n\n");
+				menu_choice = 0;
+				break;
+			}
+		}	
+		
+	} while(menu_choice == 0);
 
+    return 0;
+}
+
+// Questa funzione visualizza i file contenuti nella directory
+void printContentDirectory() {
 	// Crea un puntatore di tipo DIR
     DIR *d;
   	struct dirent *dir; 
   	d = opendir(".");
+  	
+  	printf("\n//// DIRECTORY ////\n");
   	if (d) {
   		
     	while ((dir = readdir(d)) != NULL) {
     		
       		printf("%s\n", dir->d_name);
     	}
+    	
+    printf("///////////////////\n\n");
     closedir(d);
   	}
-    return 0;
 }
